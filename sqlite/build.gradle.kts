e9d6739a0ec58c1e3b8c442d6736e78a07bb496c
David Groomes <dwgroomes@gmail.com>
2023-01-28 13:24:59 -0600

    [`sqlite/`] Gradle version catalog (TOML); clean ups

--- sqlite/build.gradle.kts
+++ sqlite/build.gradle.kts
@@ -6,16 +6,12 @@ repositories {
     mavenCentral()
 }
 
-val slf4jVersion = "1.7.36" // SLF4J releases: http://www.slf4j.org/news.html
-val sqliteVersion = "3.36.0.3" // SQLite JDBC releases: https://github.com/xerial/sqlite-jdbc/releases
-val hibernateVersion = "6.1.1.Final" // Hibernate ORM releases: https://hibernate.org/orm/releases/
-
 dependencies {
-    implementation("org.slf4j:slf4j-api:$slf4jVersion")
-    implementation("org.slf4j:slf4j-simple:$slf4jVersion")
-    implementation("org.xerial:sqlite-jdbc:$sqliteVersion")
-    implementation("org.hibernate.orm:hibernate-core:$hibernateVersion")
-    implementation("org.hibernate.orm:hibernate-community-dialects:$hibernateVersion")
+    implementation(libs.slf4j.api)
+    implementation(libs.slf4j.simple)
+    implementation(libs.sqlite)
+    implementation(libs.hibernate.core)
+    implementation(libs.hibernate.community.dialects)
 }
 
 application {
